79a078b906cbbee88d3318bd78d3c3c7cfd00783
Daniela Melo <ultradan@protonmail.com>
2021-12-28 18:13:07 -0500

    feat: seeders factories

--- database/migrations/2021_12_28_225744_create_role_user_table.php
+++ database/migrations/2021_12_28_225744_create_role_user_table.php
@@ -15,6 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,6 +13,12 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->insert([
+            'name' => 'Admin'
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'Customer'
+        ]);
     }
 }
