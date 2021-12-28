07acf3d9561103f1b3988496d23411cc8b447e06
Daniela Melo <ultradan@protonmail.com>
2021-12-28 18:02:31 -0500

    feat: web route updated

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/admin/users', [UserController::class, 'index'])->name('admin.index')->middleware('is_admin');
-
+/*
 Route::prefix('admin')->name('admin.')->group(function(){
     Route::get('/', function(){
         return view('admin.flag');
         })->middleware('is_admin');
-});
+});*/
