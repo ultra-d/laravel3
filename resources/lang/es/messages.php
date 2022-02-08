3ff6eb933e0def453098a182273d1a39fdbb2329
Daniela Melo <ultradan@protonmail.com>
2022-02-07 20:47:22 -0500

    style: update lang for translations

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -11,5 +11,10 @@ return [
         'verify_email' => 'Verificacion de Correo Electrónico',
         'verify_email_text' => 'Antes de proceder, debes verificar tu cuenta, por favor revisa en tu correo el link de verificación:',
         'verify_email_resend' => 'Para reenviar el link de verificación',
+    ],
+    'success' => [
+        'product_updated' => 'El producto fue actualizado exitosamente.',
+        'product_deleted' => 'El producto fue eliminado exitosamente.',
+        'product_created' => 'El producto fue creado exitosamente.',
     ]
 ];
