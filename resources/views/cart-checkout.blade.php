b2b9059fa8f9be04cd86054e86f89f9461a9b57f
Daniela Melo <ultradan@protonmail.com>
2022-03-31 00:22:35 -0500

    feat: add route to connect webcheckout and modify passed data

--- resources/views/cart-checkout.blade.php
+++ resources/views/cart-checkout.blade.php
@@ -55,12 +55,14 @@
             <!-- Buyer billing payment data  -->
             <div class="col-md-7 col-lg-8">
                 <h4 class="mb-3">Billing address</h4>
-                <form class="needs-validation" novalidate>
+                <form class="needs-validation" action="{{route('payment.create.session')}}" method="post" novalidate>
+                    @csrf
                     <div class="row g-3">
 
                         <div class="col-sm-6">
                             <label for="firstName" class="form-label">First name</label>
-                            <input type="text" class="form-control" id="firstName" placeholder="" value=" {{ auth()->user()->name }}" required>
+                            <input type="text" class="form-control" id="firstName" 
+                                name="buyer['name']" placeholder="Your name" value="{{ auth()->user()->name}}" required>
                             <div class="invalid-feedback">
                                 Valid first name is required.
                             </div>
@@ -68,7 +70,8 @@
                     
                         <div class="col-sm-6">
                             <label for="lastName" class="form-label">Last name</label>
-                            <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                            <input type="text" class="form-control" id="lastName" 
+                                name="buyer['surname']" placeholder="Your Last name" value="" required>
                             <div class="invalid-feedback">
                                 Valid last name is required.
                             </div>
@@ -76,7 +79,8 @@
                         
                         <div class="col-12">
                             <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                            <input type="email" class="form-control" id="email" placeholder="you@example.com"  value=" {{ auth()->user()->email }}">
+                            <input type="email" class="form-control" id="email" 
+                                name="buyer['email']" placeholder="you@example.com"  value="{{ auth()->user()->email}}">
                             <div class="invalid-feedback">
                                 Please enter a valid email address for shipping updates.
                             </div>
@@ -84,22 +88,17 @@
                         
                         <div class="col-12">
                             <label for="address" class="form-label">Address</label>
-                            <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+                            <input type="text" class="form-control" id="address" 
+                                name="buyer['address']['street']" placeholder="1234 Main St" required>
                             <div class="invalid-feedback">
                                 Please enter your shipping address.
                             </div>
                         </div>
-                        
-                        <div class="col-12">
-                            <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                            <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-                        </div>
-                        
+                    
                         <div class="col-md-5">
                             <label for="country" class="form-label">Country</label>
-                            <select class="form-select" id="country" required>
-                                <option value="">Choose...</option>
-                                <option>Colombia</option>
+                            <select class="form-select" id="country" name="buyer['address']['country']" required>
+                                <option value="CO" selected>Colombia</option>
                             </select>
                             <div class="invalid-feedback">
                                 Please select a valid country.
@@ -108,9 +107,10 @@
                         
                         <div class="col-md-4">
                             <label for="state" class="form-label">State</label>
-                            <select class="form-select" id="state" required>
-                                <option value="">Choose...</option>
-                                <option>Norte de Santander</option>
+                            <select class="form-select" id="state" name="buyer['address']['state']" required>
+                                <option value="Norte de Santander" selected>Norte de Santander</option>
+                                <option value="Santander">Santander</option>
+                                <option value="Antioquia">Antioquia</option>
                             </select>
                             <div class="invalid-feedback">
                                 Please provide a valid state.
@@ -119,7 +119,7 @@
                         
                         <div class="col-md-3">
                             <label for="zip" class="form-label">Zip</label>
-                            <input type="text" class="form-control" id="zip" placeholder="" required>
+                            <input type="text" class="form-control" id="zip" name="buyer['address']['postalCode']" placeholder="">
                             <div class="invalid-feedback">
                                 Zip code required.
                             </div>
@@ -128,71 +128,7 @@
                     
                     <hr class="my-4">
                     
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="same-address">
-                        <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-                    </div>
                     
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="save-info">
-                        <label class="form-check-label" for="save-info">Save this information for next time</label>
-                    </div>
-                    
-                    <hr class="my-4">
-                    
-                    <h4 class="mb-3">Payment</h4>
-                    
-                    <div class="my-3">
-                        <div class="form-check">
-                            <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
-                            <label class="form-check-label" for="credit">Credit card</label>
-                        </div>
-                        <div class="form-check">
-                            <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
-                            <label class="form-check-label" for="debit">Debit card</label>
-                        </div>
-                        <div class="form-check">
-                            <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
-                            <label class="form-check-label" for="paypal">PayPal</label>
-                        </div>
-                    </div>
-                    
-                    <div class="row gy-3">
-                        <div class="col-md-6">
-                            <label for="cc-name" class="form-label">Name on card</label>
-                            <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                            <small class="text-muted">Full name as displayed on card</small>
-                            <div class="invalid-feedback">
-                                Name on card is required
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-6">
-                            <label for="cc-number" class="form-label">Credit card number</label>
-                            <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                            <div class="invalid-feedback">
-                                Credit card number is required
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-3">
-                            <label for="cc-expiration" class="form-label">Expiration</label>
-                            <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                            <div class="invalid-feedback">
-                                Expiration date required
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-3">
-                            <label for="cc-cvv" class="form-label">CVV</label>
-                            <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                            <div class="invalid-feedback">
-                                Security code required
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <hr class="my-4">
                     
                     <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
                 </form>
