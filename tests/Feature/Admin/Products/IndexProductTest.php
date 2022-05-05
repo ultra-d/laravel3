b2ab3a37a6fb5bccf8bfda5ed4b07a55b31c004f
Daniela Melo <ultradan@protonmail.com>
2022-05-04 20:15:51 -0500

    test: modify code in admin products testing

--- tests/Feature/Admin/Products/IndexProductTest.php
+++ tests/Feature/Admin/Products/IndexProductTest.php
@@ -10,7 +10,7 @@ class IndexProductTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_only_admin_can_see_product_index(): void
+    public function testOnlyAdminManagementProductsView(): void
     {
         $user = User::factory()->hasRoles(0, ['name' => 'Customer'])->create();
         $response = $this->actingAs($user)->get(route('admin.products.index'));
