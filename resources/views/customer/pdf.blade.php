56664c599c973afbf2ee2d3d1c711d812fa1b2bc
Daniela Melo <ultradan@protonmail.com>
2022-05-15 22:33:03 -0500

    style: erase commented code

--- resources/views/customer/pdf.blade.php
+++ resources/views/customer/pdf.blade.php
@@ -1,11 +1,4 @@
-{{-- <head>
-    <meta charset="utf-u">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>PRUEBA PDF</title>
-</head> --}}
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <title>{{ $invoice->reference }}</title>
