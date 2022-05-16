cedc0641edb30ba3c08232a52e97444e94c22c2f
Daniela Melo <ultradan@protonmail.com>
2022-05-15 22:23:09 -0500

    feat: create schedule event to consult invoice status

---
+++ app/Console/Commands/ConsultInvoiceStatusCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\GetInvoiceInformation;
+use App\Models\Invoice;
+use Illuminate\Console\Command;
+use App\Constants\PaymentStatus;
+use Illuminate\Support\Facades\Log;
+
+class ConsultInvoiceStatusCommand extends Command
+{
+
+    protected $signature = 'get:status';
+
+    protected $description = 'Get invoice status (PENDING) to APPROVE OR FAILED';
+
+    public function handle(): void
+    {
+        $invoices = Invoice::where('payment_status', PaymentStatus::PENDING)->get();
+
+        if ($invoices->isNotEmpty()) {
+            foreach ($invoices as $invoice) {
+                Log::info($invoice->id . ' ' . $invoice->payment_status);
+                (new GetInvoiceInformation())->getInformation($invoice);
+            }
+        }
+    }
+}
